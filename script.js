0d79d425cb2810116349ea1c3e6b4d92c6dae44f
Imran shah <94097713+imranshah007@users.noreply.github.com>
2023-02-21 18:03:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,137 @@
-const enterDate = document.getElementById("enterDate");
-let realDate = document.getElementsByTagName("td");
-function date() {
-  for (let i = 0; i < realDate.length; i++) {
-    enterDate.value === realDate[i].innerText
-      ? realDate[i].style.backgroundColor === "white"
-        ? (realDate[i].style.backgroundColor = "green")
-        : (realDate[i].style.backgroundColor = "white")
-      : (realDate[i].style.backgroundColor = "white");
+function generate_year_range(start, end) {
+  let years = "";
+  for (let year = start; year <= end; year++) {
+    years += "<option value='" + year + "'>" + year + "</option>";
   }
+  return years;
+}
+let today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+selectYear = document.getElementById("year");
+selectMonth = document.getElementById("month");
+createYear = generate_year_range(1970, 2050);
+document.getElementById("year").innerHTML = createYear;
+let calendar = document.getElementById("calendar");
+let lang = calendar.getAttribute("data-lang");
+let months = "";
+let days = "";
+let myDays;
+
+const monthDefault = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const dayDefault = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+if (lang == "en") {
+  months = monthDefault;
+  days = dayDefault;
 }
 
-function dateTwo() {
-  if (event.key === "Enter") {
-    for (let i = 0; i < realDate.length; i++) {
-      enterDate.value === realDate[i].innerText
-        ? realDate[i].style.backgroundColor === "white"
-          ? (realDate[i].style.backgroundColor = "green")
-          : (realDate[i].style.backgroundColor = "white")
-        : (realDate[i].style.backgroundColor = "white");
+let $dataHead = "<tr>";
+for (dhead in days) {
+  $dataHead += "<th data-days='" + days[dhead] + "'>" + days[dhead] + "</th>";
+}
+$dataHead += "</tr>";
+document.getElementById("thead-month").innerHTML = $dataHead;
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+function next() {
+  currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+  currentMonth = (currentMonth + 1) % 12;
+  showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+  currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+  showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+  currentYear = parseInt(selectYear.value);
+  currentMonth = parseInt(selectMonth.value);
+  showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+  let firstDay = new Date(year, month).getDay();
+  tbl = document.getElementById("calendar-body");
+  tbl.innerHTML = "";
+  monthAndYear.innerText = months[month] + " " + year;
+  selectYear.value = year;
+  selectMonth.value = month;
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement("tr");
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < firstDay) {
+        cell = document.createElement("td");
+        cellText = document.createTextNode("");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      } else if (date > daysInMonth(month, year)) {
+        break;
+      } else {
+        cell = document.createElement("td");
+        cell.setAttribute("data-date", date);
+        cell.setAttribute("data-month", month + 1);
+        cell.setAttribute("data-year", year);
+        cell.setAttribute("data-month_name", months[month]);
+        cell.className = `date-picker ${date}`;
+        cell.innerHTML = date;
+        if (
+          date === today.getDate() &&
+          year === today.getFullYear() &&
+          month === today.getMonth()
+        ) {
+          cell.className = "date-picker selected";
+        }
+        row.appendChild(cell);
+        date++;
+      }
     }
+    tbl.appendChild(row);
   }
 }
 
-function changeState() {
-  const select = document.getElementById("month");
-
-  let state = select.value;
-  document.getElementById("mon").innerText = state;
+function date5(myDays) {
+  const value = document.getElementById("value").value;
+  const ele = document.getElementsByClassName(value);
+  const allEle = document.getElementsByClassName("sel");
+  const all = document.getElementsByClassName("date-picker");
+  if (value >= myDays || value < 1) {
+    alert("Please enter a valid date!");
+  }
+  if (all[value - 1].innerHTML == value && allEle.length >= 1) {
+    allEle[0].classList.remove("sel");
+    all[value - 1].className += " sel";
+  } else {
+    if(all[value-1].className.split(" ").includes("sel")){
+   all[value-1].classList.remove("sel");
+   all[value-1].className += " white";
+    }
+    all[value - 1].className += " sel";
+  }
+}
+const btn = document.getElementsByClassName("button1");
+btn[0].addEventListener("click", () => date5(myDays));
+const elements = document.getElementsByTagName("td");
+function daysInMonth(iMonth, iYear) {
+  myDays = 32 - new Date(iYear, iMonth, 32).getDate();
+  return myDays;
 }
 
-function changeYear() {
-  const currentYear = document.getElementById("year");
-
-  let state = currentYear.value;
-  document.getElementById("yr").innerText = state;
-}
